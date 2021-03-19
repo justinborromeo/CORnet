24796f5979013797dae9ce2020d9b845b238e66b
Justin Borromeo <jborrome@gra-login1.graham.sharcnet>
2021-03-19 19:51:18 -0400

    .

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ import cornet
 from PIL import Image
 Image.warnings.simplefilter('ignore')
 
-os.system("taskset -p 0xffffffff %d" % os.getpid())
+os.system("taskset -p 0xfffffff %d" % os.getpid())
 
 np.random.seed(0)
 torch.manual_seed(0)
